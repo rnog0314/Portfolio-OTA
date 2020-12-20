a931a4fe2e5d3daaa52124d02a5b0105a392e10c
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2020-12-21 08:24:43 +0900

    header.htmlの作成に取り掛かり中。その完了に向けて再開

--- portfolio/src/main/java/com/example/portfolio/controller/IndexController.java
+++ portfolio/src/main/java/com/example/portfolio/controller/IndexController.java
@@ -3,15 +3,17 @@ package com.example.portfolio.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/portfolio")
 public class IndexController {
 
   @GetMapping("")
-  public String index() {
+  public String index(Model m) {
     
     return "index";
   }
+
 
 }
