76c5db463519eee7e805a61ea1a8a56fffc34b62
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-01-15 23:56:23 +0900

    Mockの学習中

--- portfolio/src/test/java/com/example/portfolio/controller/AdminControllerTest.java
+++ portfolio/src/test/java/com/example/portfolio/controller/AdminControllerTest.java
@@ -3,6 +3,9 @@ package com.example.portfolio.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import javax.transaction.Transactional;
@@ -12,9 +15,14 @@ import com.example.portfolio.model.session.AdminSession;
 // import com.example.portfolio.model.entity.Admin;
 import com.example.portfolio.service.AdminService;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
@@ -22,13 +30,14 @@ import org.springframework.test.context.jdbc.Sql;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 @Sql
+@ExtendWith(MockitoExtension.class)
 public class AdminControllerTest {
 
-  @Autowired
-  private AdminService adminService;
+  @InjectMocks
+  private AdminController adminController = new AdminController();
 
   @Mock
-  AdminSession adminSession;
+  private AdminService adminService;
 
   /**
    * 管理者画面ログイン
@@ -38,26 +47,56 @@ public class AdminControllerTest {
 
     /** 管理者の存在チェック */
     // 管理者アカウントが存在する場合
-    String adminName = "testAdmin1";
-    String password = "password001";
-    Admin admin1 = adminService.findByAdminNameAndPassword(adminName, password);
-    assertTrue(admin1 != null);
+    // String adminName = "testAdmin1";
+    // String password = "password001";
+    // Admin expected = new Admin(1, "testAdmin1", "password001");
+    // when(adminService.findByAdminNameAndPassword(anyString(), anyString())).thenReturn(expected);
+    // Admin actual = adminService.findByAdminNameAndPassword(adminName, password);
+    // assertEquals(expected.getAdminName(), adminName);
+
+
+    // Declare variables
+    // String adminName;
+    // String password;
+    // String actual;
+    // String expected;
+
+    // // Provide initial data
+    // @BeforeEach
+    // public void setup() {
+
+    // }
+
+    // // Do unit test
+    // @Test
+    // public void testFindByAdminNameAndPassword() {
+
+    // }
+    // // clean heap data
+    // @AfterEach
+    // public void clean() {
+
+    // }
+
+    /** 管理者の存在チェック */
+    // 管理者アカウントが存在する場合
+    // String adminName = "testAdmin1";
+    // String password = "password001";
+    // Admin admin1 = adminService.findByAdminNameAndPassword(adminName, password);
+    // assertTrue(admin1 != null);
 
     // 管理者アカウントが存在しない場合
-    adminName = "adminNotExsit";
-    password = "passwordNotExsit";
-    Admin admin2 = adminService.findByAdminNameAndPassword(adminName, password);
-    assertTrue(admin2 == null);
+    // adminName = "adminNotExsit";
+    // password = "passwordNotExsit";
+    // Admin admin2 = adminService.findByAdminNameAndPassword(adminName, password);
+    // assertTrue(admin2 == null);
 
     /** adminSessionへの登録 */
 
-    Admin admin3 = new Admin(1, "testAdmin2", "password002");
-    adminService.setAdminSession(admin3);
+    // Admin admin3 = new Admin(1, "testAdmin2", "password002");
+    // adminService.setAdminSession(admin3);
 
-    adminSession.setId(1);
-    adminSession.setAdminName("testAdmin");
-    adminSession.setPassword("password2");
-    adminSession.setLogined(true);
+    // AdminSession adminSession = mock(AdminSession.class);
   }
 
 }
