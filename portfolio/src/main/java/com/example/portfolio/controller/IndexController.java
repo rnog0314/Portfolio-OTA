5b14e7825d7c6f486f4643f177ebc5b5a641a5a6
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-01-19 07:47:44 +0900

    TODO更新

--- portfolio/src/main/java/com/example/portfolio/controller/IndexController.java
+++ portfolio/src/main/java/com/example/portfolio/controller/IndexController.java
@@ -69,7 +69,6 @@ public class IndexController {
   - JSファイルの分離
   - 各ページのimgのbdrsの修正
   - Repositoryの単体テスト(更新系はBeanPropertyRowMapperを使ってDBの状態確認)
-  - products/usersのpk変更によるリグレッションテストをこのブランチで行う
 */
 
 /* 残りの作成工程
