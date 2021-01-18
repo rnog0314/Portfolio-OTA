7aba2ab80f02ec8986223788ffb7593f8d039704
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-01-18 20:52:59 +0900

    NoticeRepositoryTest

---
+++ portfolio/src/test/java/com/example/portfolio/repository/NoticeRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.example.portfolio.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.example.portfolio.model.dao.NoticeRepository;
+import com.example.portfolio.model.entity.Notice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+@Sql
+public class NoticeRepositoryTest {
+
+  @Autowired
+  private NoticeRepository repos;
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @Test
+  public void testUpdate() {
+    // 更新処理
+    int id = 1;
+    String title = "updatedTitle";
+    String text = "updatedText";
+    repos.update(id, title, text);
+
+    // 更新確認
+    String sql = "SELECT * FROM notices WHERE id = ?";
+    RowMapper<Notice> rowMapper = new BeanPropertyRowMapper<Notice>(Notice.class);
+    Notice notice = jdbcTemplate.queryForObject(sql, rowMapper, id);
+    assertEquals("updatedTitle", notice.getTitle());
+    assertEquals("updatedText", notice.getText());
+  }
+
+  @Test
+  public void testFindAllByVisibleFlagTrueOrderByCreatedAt() {
+      List<Notice> actual = repos.findAllByVisibleFlagTrueOrderByCreatedAt();
+      assertEquals(1, actual.size());
+  }
+}
