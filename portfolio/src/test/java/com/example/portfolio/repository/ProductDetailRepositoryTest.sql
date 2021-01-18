424cd6446e02c483846f31a21c9815d51188504b
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-01-18 21:21:39 +0900

    ProductDetailRepositoryTest

---
+++ portfolio/src/test/java/com/example/portfolio/repository/ProductDetailRepositoryTest.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (category_name, category_image) VALUES ('dummyCategoryName', 'dummyCategoryImgPath');
+
+INSERT INTO destinations (destination_name, destination_image) VALUES ('dummyDestinationName', 'dummyDestinationImgPath');
+
+INSERT INTO products (product_name, category_id, product_image, destination_id, price) VALUES ('dummyProductName', 1, 'dummyProductImgPath', 1, 1000);
+
+INSERT INTO product_details (product_id, image, article_title, article_text, price) VALUES (1, 'dummyImg', 'dummyArticleTitle', 'dummyArticleText', 1000);
