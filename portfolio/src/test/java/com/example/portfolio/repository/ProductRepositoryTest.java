eae8a3c784709f0ff601b8d07f537bc6848b11de
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-01-18 22:14:35 +0900

    testFindByCategoryId

--- portfolio/src/test/java/com/example/portfolio/repository/ProductRepositoryTest.java
+++ portfolio/src/test/java/com/example/portfolio/repository/ProductRepositoryTest.java
@@ -12,6 +12,9 @@ import com.example.portfolio.model.entity.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,10 +25,31 @@ public class ProductRepositoryTest {
   @Autowired
   private ProductRepository repos;
 
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
   @Test
   public void testFindByDestinationId() {
     int id = 1;
     List<Product> actual = repos.findByDestinationId(id);
     assertEquals(2, actual.size());
+  }
+
+  @Test
+  public void testFindByCategoryId() {
+    int id = 3;
+    List<Product> actual = repos.findByCategoryId(id);
+    assertEquals(2, actual.size());
+  }
+
+  @Test
+  public void testUpdateProduct() {
+    String productName = "updatedName";
+    int price = 2000;
+    repos.updateProduct(productName, price);
+    String sql = "SELECT * FROM admin";
+    RowMapper<Product> rowMapper = new BeanPropertyRowMapper<Product>(Product.class);
+    List<Product> admin = jdbcTemplate.query(sql, rowMapper);
+
   }
 }
--- portfolio/src/test/java/com/example/portfolio/repository/ProductRepositoryTest.sql
+++ portfolio/src/test/java/com/example/portfolio/repository/ProductRepositoryTest.sql
@@ -10,6 +10,7 @@ INSERT INTO destinations (destination_id, destination_name) VALUES (2, 'dummyDes
 INSERT INTO products (product_id, product_name, product_image, category_id, destination_id, delete_flag, price) VALUES (1, 'dummyName1', 'dummyImage1',  1, 1, false, 1000);
 INSERT INTO products (product_id, product_name, product_image, category_id, destination_id, delete_flag, price) VALUES (2, 'dummyName2', 'dummyImage2', 2, 2, false, 1000);
 INSERT INTO products (product_id, product_name, product_image, category_id, destination_id, delete_flag, price) VALUES (3, 'dummyName3', 'dummyImage3', 3, 1, false, 1000);
+INSERT INTO products (product_id, product_name, product_image, category_id, destination_id, delete_flag, price) VALUES (4, 'dummyName4', 'dummyImage4', 3, 2, false, 1000);
 
 INSERT INTO bookmarks (user_id, product_id) VALUES (1, 1);
 INSERT INTO bookmarks (user_id, product_id) VALUES (1, 2);
