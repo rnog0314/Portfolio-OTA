27d566be9a9e7baeeab53f4b0ef0f3826115205a
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-01-21 21:12:52 +0900

    category_detail.htmlのjs分離

---
+++ portfolio/src/main/resources/static/js/category_detail.js
@@ -0,0 +1,34 @@
+/*<![CDATA[*/
+$(document).ready(function () {
+  // idをつけてそれ用の初期化を記述すれば複数のowe-carouselができる(https://support.fruitfulcode.com/hc/en-us/articles/115004837994-Multiple-carousels-at-one-page)
+  $("#category-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+    },
+  });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
+
+/*]]>*/
