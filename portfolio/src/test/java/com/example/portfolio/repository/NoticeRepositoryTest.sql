72972f1d8b208156eccc7eb6ae291cf32861e2d5
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-02-08 20:31:35 +0900

    SQLファイルをテストの「repository」フォルダから削除

--- portfolio/src/test/java/com/example/portfolio/repository/NoticeRepositoryTest.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO notices (id, title, text, visible_flag) VALUES (1, 'dummyTitle', 'dummytext', true);
-INSERT INTO notices (id, title, text, visible_flag) VALUES (2, 'dummyTitle2', 'dummytext2', false);
